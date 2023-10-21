eacb63227dfb36ff0632755d668a2e739323ccdb
Franklin Roa Guerrero <franklinroaguerrero@gmail.com>
2023-10-21 16:36:43 -0500

    Initial Amplify Setup

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "calbuddy19c026db": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
